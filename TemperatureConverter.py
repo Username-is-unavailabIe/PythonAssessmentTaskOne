dba8b1335bf12474b5f8889ed18c4c302cc2a365
Username-is-unavailabIe <88470088+Username-is-unavailabIe@users.noreply.github.com>
2023-10-20 12:41:20 +1100

    Update TemperatureConverter.py

--- TemperatureConverter.py
+++ TemperatureConverter.py
@@ -26,3 +26,18 @@ else:
   #If neither option is selected it is invalid
   print("Invalid Input")
 print("Thankyou for using this program")
+
+import random
+from time import sleep
+
+question = input("Please ask your question for the 8Ball: ")
+answers = [
+    "Yes", "No", "Stars say no", "Certainly", "Why Not!", "Definitely Not"
+]
+
+ANS = random.randint(1, len(answers) - 1)
+sleep(1)
+print("...")
+
+print(f"The answer to '{question}' is {answers[ANS]}")
+
